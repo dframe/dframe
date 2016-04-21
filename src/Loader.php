9998a768b9ecbe42a9fe7c45737c3e655df1b624
Slawek Kaleta <s.kaleta@citycore.pro>
2016-04-21 11:24:32 +0200

    Tworzenie domyślnego parametru get

--- src/Loader.php
+++ src/Loader.php
@@ -1,9 +1,10 @@
 <?php
 namespace Dframe;
+use Dframe\Config;
 /**
 * System Ładowania plików Kontrollera
 */
-include "Functions.php";
+include_once "Functions.php";
 
 class Loader extends Core
 {
@@ -18,8 +19,19 @@ class Loader extends Core
         
         $this->router->parseGets(); // Prasowanie $_GET
 
+        $routerConfig = Config::load('router');
+
+        if(empty($_GET['task']))
+        	$_GET['task'] = $routerConfig->get('NAME_CONTROLLER');
+
+        if(empty($_GET['action']))
+        	$_GET['action'] = $routerConfig->get('NAME_MODEL');
+
+
+
         $this->controller = $_GET['task'];
         $this->action = $_GET['action'];
+
 
         if(strstr($this->controller, ",") !== False){
 
@@ -32,46 +44,54 @@ class Loader extends Core
             $this->controller = $url[$urlCount];
 
         }else $subControler = null;
+
+
         
 
        // Does the class exist?
         $patchController = 'Controller/'.$subControler.''.$this->controller.'.php';
+        //var_dump($patchController);
         if(file_exists($patchController)){
+            //echo 'OK 1';
             include_once $patchController;
+            //echo 'OK 2';
             $path = null;
         }
 
-        if(!class_exists('\Controller\\'.$this->controller.'Controller')){
+        $xsubControler = str_replace("/", "\\", $subControler);
+        
+        if(!class_exists('\Controller\\'.$xsubControler.''.$this->controller.'Controller')){
         	if(ini_get('display_errors') == "on"){
-        		echo 'Patch: '.$patchController;
-        		echo 'bad controller error';
+        		echo 'Patch: '.'Controller/'.$xsubControler.''.$this->controller.'Controller';
+        		echo '<br>bad controller error<br>';
         		return 1;
         	}
         	header("HTTP/1.0 404 Not Found");
             return 1;
         }
-            
-        $parents = class_parents('\Controller\\'.$this->controller.'Controller');
-        // Does the class extend the controller class?
-        if(!method_exists('\Controller\\'.$this->controller.'Controller',$this->action)) {
-            if(!method_exists('\Controller\\'.$this->controller.'Controller', 'page')){
-
-            	if(ini_get('display_errors') == "on"){
-            		echo 'Brak metody';
-            		return 1;
-            	}
-          
-            	header("HTTP/1.0 404 Not Found");
-            	header('Location: ./?notFound=1');
-    
-                return 1;
-
-
-            }
-
-        }
-
-        $this->controller = '\Controller\\'.$this->controller.'Controller';
+        //    
+        //$parents = class_parents('Scscript\Controller\\'.$this->controller.'Controller');
+        //// Does the class extend the controller class?
+        //if(!method_exists('Scscript\Controller\\'.$this->controller.'Controller',$this->action)) {
+        //    if(!method_exists('Scscript\Controller\\'.$this->controller.'Controller', 'page')){
+//
+        //    	if(ini_get('display_errors') == "on"){
+        //    		echo 'Brak metody';
+        //    		return 1;
+        //    	}
+        //  
+        //    	header("HTTP/1.0 404 Not Found");
+        //    	header('Location: ./?notFound=1');
+    //
+        //        return 1;
+//
+//
+        //    }
+//
+        //}
+        
+        
+        $this->controller = '\Controller\\'.$xsubControler.''.$this->controller.'Controller';
         return new $this->controller($this->baseClass);
     }
 
