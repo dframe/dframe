e7051548bed31da9d348fea105e98ad155115b3e
Slawek Kaleta <s.kaleta@citycore.pro>
2016-06-22 18:02:13 +0200

    Poprawa bazy

--- src/Database/WhereChunkString.class.php
+++ src/Database/WhereChunkString.class.php
@@ -5,12 +5,13 @@ class WhereStringChunk
     public $string;
     public $bindWhere;
 
-    function __construct($string, $bindWhere) {
+    function __construct($string, $bindWhere = null) {
         $this->string = $string;
         $this->bindWhere = $bindWhere;
     }
+    
     function build() {
-        return array($this->string, $bindWhere);
+        return array($this->string, $this->bindWhere);
     }
 }
 
