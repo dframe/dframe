d4e7a1de3f45c928afb9f549b87643bc54983770
Slawek Kaleta <s.kaleta@citycore.pro>
2017-04-09 15:02:08 +0200

    Separated example, update composer.json, prototype router response

--- example/app/View/templates/index.html.php
+++
@@ -1,9 +0,0 @@
-{include file="header.html.php"}
-
-Przykładowa strona stworzona na Frameworku Dframe  | <b>Plik:</b> View/templates/index.html.php <br>
-
-<br><br>
-Routing: <br>
-{$router->makeurl('page/index')}
-
-{include file="footer.html.php"}
---
+++ src/Router/Response.php
@@ -0,0 +1,114 @@
+<?php
+namespace Dframe\Router;
+use Dframe\Config;
+
+/**
+ * Copyright (C) 2016  Sławomir Kaleta
+ * @author Sławomir Kaleta
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+class Response
+{
+
+    public $status = 200;
+
+    public $headers = array();
+
+    public function code = array(
+        100 => 'Continue',
+        101 => 'Switching Protocols',
+        102 => 'Processing',
+        200 => 'OK',
+        201 => 'Created',
+        202 => 'Accepted',
+        203 => 'Non-Authoritative Information',
+        204 => 'No Content',
+        205 => 'Reset Content',
+        206 => 'Partial Content',
+        207 => 'Multi-Status',
+        300 => 'Multiple Choices',
+        301 => 'Moved Permanently',
+        302 => 'Found',
+        303 => 'See Other',
+        304 => 'Not Modified',
+        305 => 'Use Proxy',
+        306 => 'Switch Proxy',
+        307 => 'Temporary Redirect',
+        400 => 'Bad Request',
+        401 => 'Unauthorized',
+        402 => 'Payment Required',
+        403 => 'Forbidden',
+        404 => 'Not Found',
+        405 => 'Method Not Allowed',
+        406 => 'Not Acceptable',
+        407 => 'Proxy Authentication Required',
+        408 => 'Request Timeout',
+        409 => 'Conflict',
+        410 => 'Gone',
+        411 => 'Length Required',
+        412 => 'Precondition Failed',
+        413 => 'Request Entity Too Large',
+        414 => 'Request-URI Too Long',
+        415 => 'Unsupported Media Type',
+        416 => 'Requested Range Not Satisfiable',
+        417 => 'Expectation Failed',
+        418 => 'I\'m a teapot',
+        422 => 'Unprocessable Entity',
+        423 => 'Locked',
+        424 => 'Failed Dependency',
+        425 => 'Unordered Collection',
+        426 => 'Upgrade Required',
+        449 => 'Retry With',
+        450 => 'Blocked by Windows Parental Controls',
+        500 => 'Internal Server Error',
+        501 => 'Not Implemented',
+        502 => 'Bad Gateway',
+        503 => 'Service Unavailable',
+        504 => 'Gateway Timeout',
+        505 => 'HTTP Version Not Supported',
+        506 => 'Variant Also Negotiates',
+        507 => 'Insufficient Storage',
+        509 => 'Bandwidth Limit Exceeded',
+        510 => 'Not Extended'
+    );
+
+    public function status($code){
+        $this->status = $code;
+    }
+
+    public function header($header = false){
+        $protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.1');
+        $status = (!empty($this->status) ? $this->status : 200;
+        $string = sprintf('%s %d %s', $protocol, $status, self::$code[$status]);
+
+        header($string, true, $status); // Default header
+        if(!empty($header)){
+
+            foreach ($header as $field => $value) {
+                if(is_array($value)){
+                    foreach ($value as $v) {
+                        header($field.': '.$v, false);
+                    }
+                }else{
+                    header($field.': '.$value);
+                }
+            }
+        }
+
+        return $this;
+    }
+}
