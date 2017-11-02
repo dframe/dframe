4de8dfdc4812a229f28c083af41b2bc2f3146d96
Sￅﾂawek Kaleta <s.kaleta@citycore.pro>
2017-11-02 12:19:50 +0100

    coding style - back compatibility
    define('CODING_STYLE', false);

--- src/Loader.php
+++ src/Loader.php
@@ -80,7 +80,9 @@ class Loader extends Core
         try {
 
             if (!$this->isCamelCaps($name, true)) {
-                 throw new BaseException('Camel Sensitive is on. Can not use '.$type.' '.$name.' try to use camelCaseName');
+            	if(!defined('CODING_STYLE') OR (defined('CODING_STYLE') AND CODING_STYLE == true))){
+                     throw new BaseException('Camel Sensitive is on. Can not use '.$type.' '.$name.' try to use camelCaseName');
+            	}
             }
             
             $name = !empty($folder) ? '\\'.$type.'\\'.str_replace(array('\\', '/'), '\\', $folder).$name.$type : '\\'.$type.'\\'.$name.$type;;   
