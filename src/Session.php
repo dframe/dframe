72393e510532b64a4e8a6272927c0cf38ab40a17
Slawek Kaleta <slaszka@gmail.com>
2018-01-21 13:38:11 +0100

    Secured the sessions

--- src/Session.php
+++ src/Session.php
@@ -13,23 +13,61 @@ namespace Dframe;
  *
  * @author Sławek Kaleta <slaszka@gmail.com>
  */
+
 class Session
 {
     
-    function __construct($name)
+    function __construct($name = '_sessionName', $options = [])
     {
-        if (!isset($_SESSION)) {
-            session_name($name);
-            session_start();
 
+    	$this->name = $name;
+
+    	if(!isset($_SESSION)){
+
+    		$cookie = array(
+    		    'lifetime' => isset($options['cookie']['lifetime']) ? $options['cookie']['lifetime'] : 0,
+    		    'path' => isset($options['cookie']['path']) ? $options['cookie']['path'] : '/',
+    		    'domain' => isset($options['cookie']['domain']) ? $options['cookie']['domain'] : null,
+    		    'secure' => isset($options['cookie']['secure']) ? $options['cookie']['secure'] : isset($_SERVER['HTTPS']) ? $_SERVER['HTTPS'] : null,
+    		    'httponly' => isset($options['cookie']['httponly']) ? $options['cookie']['httponly'] : false,
+    		);
+    		
+		    session_set_cookie_params($cookie['lifetime'], $cookie['path'], $cookie['domain'], $cookie['secure'], $cookie['httponly']);
+            session_name($this->name);
+            session_start();
+        }
+        
+        $this->ipAddress = isset($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+		$this->userAgent = $_SERVER['HTTP_USER_AGENT'];
+
+        if($this->isValidFingerprint() != true){
+        	// Refresh Session
+        	$_SESSION = array();
+		    $_SESSION['_fingerprint'] = $this->_getFingerprint();
         }
     }
+
+    private function _getFingerprint(){
+    	return md5($this->ipAddress.$this->userAgent.$this->name);
+    }
+
+    /**
+     * Register the session.
+     *
+     * @param integer $time.
+     */
 
     public function register($time = 60)
     {
         $_SESSION['sessionId'] = session_id();
         $_SESSION['sessionTime'] = intval($time);
     }
+
+    /**
+     * Checks if the session is registered.
+     *
+     * @return bool
+     */
 
     public function authLogin()
     {
@@ -53,11 +91,27 @@ class Session
         
         return false;
     }
-    
+
+    /**
+     * Set session key
+     *
+     * @param string $key
+     * @param string $value
+     */
+
     public function set($key, $value)
     {
         $_SESSION[$key] = $value;
     }
+
+    /**
+     * get session key
+     *
+     * @param string $key
+     * @param string $or
+     *
+     * @return string|null
+     */
 
     public function get($key, $or = null)
     {
@@ -75,6 +129,16 @@ class Session
     {
         session_destroy();
         $_SESSION = array();
+    }
+
+    public function isValidFingerprint(){
+
+    	$_fingerprint = $this->_getFingerprint();
+    	if(isset($_SESSION['_fingerprint']) AND $_SESSION['_fingerprint'] == $_fingerprint){
+    		return true;
+    	}
+
+    	return false;
     }
        
 }
