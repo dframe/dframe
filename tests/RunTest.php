5a4d76c045f07b94a5bc6cec007969cadf31a0af
Sławomir Kaleta <slaszka@gmail.com>
2018-06-18 10:38:26 +0200

    remove const

--- tests/RunTest.php
+++ tests/RunTest.php
@@ -7,15 +7,11 @@ ini_set('session.use_cookies', 0);
 
 session_start();
 
-define('APP_DIR', '');
-define('SALT', 'RaNdOmTeSt');
-
 class RunTest extends TestCase
 {
 
     public function testCreateController()
     {
-
         $testController = new TestController();
         $this->assertEquals('Hello World', $testController->testHelloWorld());
     }
@@ -24,7 +20,6 @@ class RunTest extends TestCase
 
 class TestController extends \Dframe\Controller
 {
-
     public function testHelloWorld()
     {
         return 'Hello World';
