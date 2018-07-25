16521d9aa60a8d722d15bf63ab46e0c9cdc53a1c
Sławomir Kaleta <slaszka@gmail.com>
2018-07-25 13:51:30 +0200

    Resolved #42, View change variables are ignored

--- src/View/SmartyView.php
+++ src/View/SmartyView.php
@@ -70,14 +70,13 @@ class SmartyView implements \Dframe\View\ViewInterface
      */
     public function fetch($name, $path = null)
     {
-        $smartyConfig = Config::load('view/smarty');
 
         $pathFile = pathFile($name);
         $folder = $pathFile[0];
         $name = $pathFile[1];
 
         if ($path == null) {
-            $path = $smartyConfig->get('setTemplateDir') . DIRECTORY_SEPARATOR . $folder . $name . $smartyConfig->get('fileExtension', '.html.php');
+            $path = $this->smarty->getTemplateDir(0) . DIRECTORY_SEPARATOR . $folder . $name . $smartyConfig->get('fileExtension', '.html.php');
         }
 
         try {
@@ -115,7 +114,7 @@ class SmartyView implements \Dframe\View\ViewInterface
         $name = $pathFile[1];
 
         if ($path == null) {
-            $path = $smartyConfig->get('setTemplateDir') . DIRECTORY_SEPARATOR . $folder . $name . $smartyConfig->get('fileExtension', '.html.php');
+            $path = $this->smarty->getTemplateDir(0) . DIRECTORY_SEPARATOR . $folder . $name . $smartyConfig->get('fileExtension', '.html.php');
         }
 
         try {
