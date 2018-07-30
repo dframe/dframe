d4bddf301cd24052421cf2705906b94de11507da
Sławomir Kaleta <slaszka@gmail.com>
2018-07-30 13:43:16 +0200

    Resolved #57, Add Cron module

---
+++ src/Cron/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * DframeFramework
+ * Copyright (c) Sławomir Kaleta
+ *
+ * @license https://github.com/dframe/dframe/blob/master/LICENCE (MIT)
+ */
+
+namespace Dframe\Cron;
+
+/**
+ * Config Class
+ *
+ * @author Sławomir Kaleta <slaszka@gmail.com>
+ */
+abstract class Task extends \Dframe\Controller
+{
+
+}
