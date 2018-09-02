e2d280887d4b8695199c10a37cd24e5da5434ebd
Sławomir Kaleta <slaszka@gmail.com>
2018-09-02 20:36:37 +0200

    Resolved #75, Add Prototype Cli

---
+++ src/Cli/Command.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace Dframe\Cli;
+
+class Command extends \Dframe\Loader
+{
+
+    public function start($args)
+    {
+
+        array_shift($args);
+        $endofoptions = false;
+
+        $ret = [
+            'commands' => [],
+            'options' => [],
+            'flags' => [],
+            'arguments' => [],
+        ];
+
+        while ($arg = array_shift($args)) {
+
+            // if we have reached end of options,
+            //we cast all remaining argvs as arguments
+            if ($endofoptions) {
+                $ret['arguments'][] = $arg;
+                continue;
+            }
+
+            // Is it a command? (prefixed with --)
+            if (substr($arg, 0, 2) === '--') {
+
+                // is it the end of options flag?
+                if (!isset($arg[3])) {
+                    $endofoptions = true;; // end of options;
+                    continue;
+                }
+
+                $value = "";
+                $com = substr($arg, 2);
+
+                // is it the syntax '--option=argument'?
+                if (strpos($com, '=')) {
+                    list($com, $value) = explode("=", $com, 2);
+
+                } elseif (strpos($args[0], '-') !== 0) { // is the option not followed by another option but by arguments
+                    while (strpos($args[0], '-') !== 0) {
+                        $value .= array_shift($args) . ' ';
+                    }
+                    $value = rtrim($value, ' ');
+                }
+
+                $ret['options'][$com] = !empty($value) ? $value : true;
+                continue;
+
+            }
+
+            // Is it a flag or a serial of flags? (prefixed with -)
+            if (substr($arg, 0, 1) === '-') {
+                for ($i = 1; isset($arg[$i]); $i++) {
+                    $ret['flags'][] = $arg[$i];
+                }
+                continue;
+            }
+
+            // finally, it is not option, nor flag, nor argument
+            $ret['commands'][] = $arg;
+            continue;
+        }
+
+        if (!count($ret['options']) && !count($ret['flags'])) {
+            $ret['arguments'] = array_merge($ret['commands'], $ret['arguments']);
+            $ret['commands'] = [];
+        }
+
+        return $ret;
+    }
+
+    public function run($args)
+    {
+        $start = $this->start($args);
+
+        $class = '\Command\\' . $start['commands'][0] . 'Command';
+        return call_user_func_array([new $class, $start['commands'][1]], $start['options']);
+    }
+}
