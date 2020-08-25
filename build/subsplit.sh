45baea185f6186a639ce02f30b23fbb5cee95f7b
Sławomir Kaleta <slaszka@gmail.com>
2020-08-25 21:57:06 +0200

    Resolved #151, add build split

---
+++ build/subsplit.sh
@@ -0,0 +1,6 @@
+git subsplit init https://github.com/dframe/dframe.git
+git subsplit publish --heads="master v4.1" src/Cron:https://github.com/dframe/config.git
+git subsplit publish --heads="master v4.1" src/Token:https://github.com/dframe/token.git
+git subsplit publish --heads="master v4.1" src/Session:https://github.com/dframe/session.git
+
+rm -rf .subsplit/
