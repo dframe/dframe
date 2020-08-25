85269587512db4ff91e6561d2f04e09cccbba481
Sławomir Kaleta <slaszka@gmail.com>
2020-08-26 00:47:04 +0200

    Resolved #153, fix build

--- build/subsplit.sh
+++ build/subsplit.sh
@@ -1,5 +1,5 @@
 git subsplit init https://github.com/dframe/dframe.git
-git subsplit publish --heads="master v4.1" src/Cron:https://github.com/dframe/config.git
+git subsplit publish --heads="master v4.1" src/Config:https://github.com/dframe/config.git
 git subsplit publish --heads="master v4.1" src/Token:https://github.com/dframe/token.git
 git subsplit publish --heads="master v4.1" src/Session:https://github.com/dframe/session.git
 
