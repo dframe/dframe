1b13b28b2834e239c867582fcda710103e4b9c24
Sławomir Kaleta <slaszka@gmail.com>
2020-09-06 15:21:22 +0200

    Resolved #169, Update subsplit build

--- build/subsplit.sh
+++ build/subsplit.sh
@@ -1,6 +1,7 @@
 git subsplit init https://github.com/dframe/dframe.git
 git subsplit publish --heads="master v4.1" src/Asset:https://github.com/dframe/asset.git
 git subsplit publish --heads="master v4.1" src/Config:https://github.com/dframe/config.git
+git subsplit publish --heads="master v4.1" src/Console:https://github.com/dframe/Console.git
 git subsplit publish --heads="master v4.1" src/Cron:https://github.com/dframe/cron.git
 git subsplit publish --heads="master v4.1" src/Loader:https://github.com/dframe/loader.git
 git subsplit publish --heads="master v4.1" src/Router:https://github.com/dframe/router.git
